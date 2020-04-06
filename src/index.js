703282313af01e46a8a80b8ff5c6a9806df53e05
GuiAndrew <guiandrew.pt@gmail.com>
2020-04-06 20:16:14 +0100

    Navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById("root")
 );
